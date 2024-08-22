1e56cf4c183afecc6649db356919c22285cccecd
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-08-22 11:10:30 +0100

    Added basic "run" routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(serde::Deserialize)]
+struct RunInfo {
+    code: String,
+    lang: String,
+    version: String,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -6,14 +14,20 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .service(home)
+            .service(get_run)
+            .service(post_run)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
-#[get("/")]
-async fn home() -> impl Responder {
+#[get("/run")]
+async fn get_run() -> impl Responder {
     HttpResponse::Ok().body("Hello!")
+}
+
+#[post("/run")]
+async fn post_run(info: web::Json<RunInfo>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Code: {}, Lang: {}, Version: {}", info.code, info.lang, info.version))
 }
